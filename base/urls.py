8721a92845adb965a914df4884fdd10747297fd3
Fabricio Lopes <fabriciolopss@ufmg.br>
2022-09-14 19:07:47 -0300

    Resumo:     Finalizei a parte básica de criar, editar e deletar tarefas e fiz o sistema de login/logout.
     Changes to be committed:
    	modified:   base/__pycache__/urls.cpython-310.pyc
    	modified:   base/__pycache__/views.cpython-310.pyc
    	new file:   base/templates/base/login.html
    	new file:   base/templates/base/task.html
    	new file:   base/templates/base/task_confirm_delete.html
    	new file:   base/templates/base/task_form.html
    	modified:   base/templates/base/task_list.html
    	modified:   base/urls.py
    	modified:   base/views.py
    	modified:   db.sqlite3

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='tasks')
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
+    
+    path('', TaskList.as_view(), name='tasks'),
+    path('tarefa/<int:pk>/', TaskDetail.as_view(), name='tarefa'),
+    path('criar-tarefa/', TaskCreate.as_view(), name="criar-tarefa"),
+    path('editar-tarefa/<int:pk>/', TaskUpdate.as_view(), name="editar-tarefa"),
+    path('deletar-tarefa/<int:pk>/', TaskDelete.as_view(), name="deletar-tarefa")
+    
 ]
